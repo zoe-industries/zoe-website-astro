e5a29384f7ebf4f869aa914ecd6a6452bf93e9ea
juliusdw <mail@jdew.de>
2024-08-25 16:16:08 +0200

    remove concept of blogposts, add generic posts

--- src/content/config.ts
+++ src/content/config.ts
@@ -13,17 +13,4 @@ const blog = defineCollection({
 	}),
 });
 
-const project = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
-});
-
-export const collections = { blog, project };
+export const collections = { blog };
